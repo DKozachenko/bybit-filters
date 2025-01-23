aaf600c20c7ac876053509bf640c023b8e2d761e
Diana Kozachenko <kozachenkodiana03@mail.ru>
2025-01-23 13:08:41 +0100

    created MVP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
-import { routes } from './app.routes';
+import { ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes)]
+  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(ROUTES, withHashLocation())]
 };
